d06057184ac74f9763313037f2fb7b40be700759
issam <issamyayou@gmail.com>
2021-01-18 16:09:08 +0100

    add vue-router package

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from './vue-router.vue'
 
+import App from './App.vue'
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: App },
+];
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
